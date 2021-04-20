17934e6fa549b27859cddb8cf3d56934e247d8c6
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-20 18:08:40 -0500

    se eliminan mensajes innecesarios

--- your-code/blackjack.py
+++ your-code/blackjack.py
@@ -1,14 +1,3 @@
-# ## Objetivo: sumar 21 puntos sin pasarse, pero sumar más puntos de los que tiene el crupier.
-# 
-# ## Reglas
-# ### Regla 1: el 1 puede tomar el valor de 1 o 11 segun convenga.
-# ### Regla 2: los números 10, 11 y 12 tienen un valor de 10.
-# 
-# ## Modo de juego
-# ### 1- El crupier repartirá dos cartas descubiertas a cada jugador y una a sí mismo.
-# ### 2- Cada jugador decidirá si pide más cartas o se planta con las ya repartidas.
-# ### 3- Luego el crupier jugará su mano.
-# 
 # Head
 import random
 
