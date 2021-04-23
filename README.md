039e4382dc37dd266be842c5f60f09567b872045
Lucio_Gutierrez <luciogutierrez@users.noreply.github.com>
2021-04-23 14:00:40 -0500

    Se incluyeron validacions para los inputs y nombre de usuario

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-Blackjack
-Objetivo: sumar 21 puntos sin pasarse, pero sumar más puntos de los que tiene el crupier.
+--------------------------- Blackjack ---------------------------
+
+Objetivo: el objetivo del juego es sumar 21 puntos sin pasarse
+pero además sumar más puntos de los obtenidos por el crupier.
+
 Reglas
-Regla 1: el 1 puede tomar el valor de 1 o 11 segun convenga.
-Regla 2: los números 10, 11 y 12 tienen un valor de 10.
+Regla 1: el número "1" puede tomar el valor de "1" u "11" según convenga al jugador.
+Regla 2: cada numero del "2" al "10" suman el mismo valor a la partida.
+Regla 3: los números "11" y "12" suman un valor de 10 a la partida.
+
 Modo de juego
-1- El crupier repartirá dos cartas descubiertas a cada jugador y una a sí mismo.
-2- Cada jugador decidirá si pide más cartas o se planta con las ya repartidas.
-3- Luego el crupier jugará su mano.
+1- El crupier repartirá dos cartas descubiertas al jugador y una a sí mismo.
+2- Si la suma de las cartas esta cerca de 21 puede plantarse y ceder el turno al crupier.
+   por el contrario si la suma es baja puede optar por sacar otra carta, recordando que en caso
+   de que la suma de sus cartas sea mayor a 21 pierde atomáticamente.
+3- La jugada compuesta por un "1" y un "10", "11" o "12" se conoce como Blackjack y gana automaticamnte.
+4- Si la suma de las cartas con cualquier combinación es igual a 21 gana automaticamnte.
+5- Cuando el jugador no quiera más cartas se plantará, cediendo el turno al crupier.
+6- Luego el crupier jugará su mano.
--- your-code/blackjack.py
+++ your-code/blackjack.py
@@ -1,9 +1,22 @@
 # Head
 import random
+import os
 
 # Nueva tarjeta
+
+
 def sacar_tarjeta():
-    return random.randint(1,12)
+    return random.randint(1, 12)
+
+# Limpiar pantalla
+
+
+def limpiar():
+    if os.name == 'nt':
+        os.system('CLS')
+    if os.name == 'posix':
+        os.system('clear')
+
 
 def reparto_inicial():
     gamer = []
@@ -13,6 +26,7 @@ def reparto_inicial():
     crupier.append(sacar_tarjeta())
     return gamer, crupier
 
+
 def suma_partida(partida):
     total1 = 0
     total2 = 0
@@ -21,7 +35,7 @@ def suma_partida(partida):
             total1 += num
         else:
             total1 += 10
-    if 1 in partida:            
+    if 1 in partida:
         for num in partida:
             if num == 1:
                 total2 += 11
@@ -31,58 +45,99 @@ def suma_partida(partida):
                 total2 += 10
     return total1, total2
 
+
 def jugada_nueva(partida):
     partida.append(sacar_tarjeta())
     suma1, suma2 = suma_partida(partida)
     return partida, suma1, suma2
 
+
 def cabecera():
-    print('------------- Blackjack -------------')
     print()
-    print('bienvenido')
+    print('--------------------------- Blackjack ---------------------------')
     print()
-    print('Objetivo: sumar 21 puntos sin pasarse, pero sumar más puntos de los que tiene el crupier.')
+    print('Objetivo: el objetivo del juego es sumar 21 puntos sin pasarse')
+    print('pero además sumar más puntos de los obtenidos por el crupier.')
     print()
     print('Reglas')
-    print('Regla 1: el 1 puede tomar el valor de 1 o 11 segun convenga.')
-    print('Regla 2: los números 10, 11 y 12 tienen un valor de 10.')
+    print('Regla 1: el número "1" puede tomar el valor de "1" u "11" según convenga al jugador.')
+    print('Regla 2: cada numero del "2" al "10" suman el mismo valor a la partida.')
+    print('Regla 3: los números "11" y "12" suman un valor de 10 a la partida.')
     print()
     print('Modo de juego')
     print('1- El crupier repartirá dos cartas descubiertas al jugador y una a sí mismo.')
-    print('2- El jugador decidirá si se planta con las cartas ya repartidas o si pide más cartas.')
-    print('3- Cuando el jugador no quiera más cartas se plantará, sediendo el turno al crupier.')
-    print('4- Luego el crupier jugará su mano.')
+    print('2- Si la suma de las cartas esta cerca de 21 puede plantarse y ceder el turno al crupier.')
+    print('   por el contrario si la suma es baja puede optar por sacar otra carta, recordando que en caso')
+    print('   de que la suma de sus cartas sea mayor a 21 pierde atomáticamente.')
+    print('3- La jugada compuesta por un "1" y un "10", "11" o "12" se conoce como Blackjack y gana automaticamnte.')
+    print('4- Si la suma de las cartas con cualquier combinación es igual a 21 gana automaticamnte.')
+    print('5- Cuando el jugador no quiera más cartas se plantará, cediendo el turno al crupier.')
+    print('6- Luego el crupier jugará su mano.')
     print()
     print('Menú')
     print('s - sacar nueva carta')
     print('p - plantarte con el juego actual')
-    print('c - sacar nueva carta para crupier')
     print('x - salir del juego')
-    print()    
+    print()
 
-cabecera()    
+
+limpiar()
+cabecera()
+jugador = input('Bien, empecemos... Ingresa tu alias de jugador: ')
+print()
 gamer, crupier = reparto_inicial()
 suma1_g, suma2_g = suma_partida(gamer)
 suma1_c, suma2_c = suma_partida(crupier)
-print("your game: ", gamer, suma1_g, suma2_g)
-print("home game: ", crupier)
-print()    
-respuesta=''
+turno = 'Jugador'
+print("Tus cartas", jugador+": ", gamer)
+print('Sumatoria normal:', suma1_g)
+if suma2_g != 0:
+    print('Sumatoria alternativa:', suma2_g)
+print('-------------------------------------------',)
+print("Cartas de Crupier: ", crupier)
+print('Sumatoria normal:', suma1_c)
+if suma2_g != 0:
+    print('Sumatoria alternativa:', suma2_c)
+print()
+print()
+respuesta = ''
 while respuesta != 'x':
-    respuesta = input('¿que deseas hacer?: ')
-    if respuesta == 's':
+    if turno == 'Jugador':
+        print('Tu turno', jugador)
+        respuesta = input('¿que deseas hacer?: ')
+    else:
+        print('Tu turno Crupier')
+        respuesta = input('¿que deseas hacer?: ')
+
+    if respuesta not in 'spx':
+        print('Las únicas respuestas validas son las letras "s", "p" o "x"')
+        continue
+
+    if respuesta == 'p':
+        respuesta = 's'
+        turno = 'Crupier'
+
+    if respuesta == 's' and turno == 'Jugador':
         gamer, suma1_g, suma2_g = jugada_nueva(gamer)
-        print("your game: ", gamer, suma1_g, suma2_g)
+        # Considerar hacer función para los resultados
+        print("Tus cartas", jugador+": ", gamer)
+        print('Sumatoria normal:', suma1_g)
+        if suma2_g != 0:
+            print('Sumatoria alternativa:', suma2_g)
         if suma1_g > 21:
             print("PERDISTE")
             break
         elif suma1_g == 21 or suma2_g == 21:
             print("FELICINDADES! GANASTE")
             break
-    elif respuesta == 'p' or respuesta == 'c':
+
+    if respuesta == 's' and turno == 'Crupier':
         crupier, suma1_c, suma2_c = jugada_nueva(crupier)
         print()
-        print("home game: ", crupier, suma1_c, suma2_c)
+        print("Cartas de Crupier: ", crupier)
+        print('Sumatoria normal:', suma1_c)
+        if suma2_c != 0:
+            print('Sumatoria alternativa:', suma2_c)
         if suma1_c > 21:
             print("La casa pierde")
             break
@@ -92,5 +147,3 @@ while respuesta != 'x':
         if (suma1_c < 22 and suma1_c > suma1_g) and suma1_c > suma2_g:
             print("La casa gana")
             break
-    else:
-        continue
